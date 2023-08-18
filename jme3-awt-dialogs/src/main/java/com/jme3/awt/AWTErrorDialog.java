59b6da8b159df514364a5473ed76fb5bc4d5fc48
riccardobl <riccardobl@users.noreply.github.com>
2023-08-18 13:38:16 +0000

    Prettified Code!

--- jme3-awt-dialogs/src/main/java/com/jme3/awt/AWTErrorDialog.java
+++ jme3-awt-dialogs/src/main/java/com/jme3/awt/AWTErrorDialog.java
@@ -44,58 +44,58 @@ import javax.swing.JTextArea;
 
 /**
  * Simple dialog for displaying error messages,
- * 
+ *
  * @author kwando
  */
 public class AWTErrorDialog extends JDialog {
-    public static String DEFAULT_TITLE = "Error in application";
-    public static int PADDING = 8;
-    
-    /**
-     * Create a new Dialog with a title and a message.
-     *
-     * @param message the message to display
-     * @param title the title to display
-     */
-    protected AWTErrorDialog(String message, String title) {
-        setTitle(title);
-        setSize(new Dimension(600, 400));
-        setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-        setLocationRelativeTo(null);   
-        
-        Container container = getContentPane();
-        container.setLayout(new BorderLayout());
-        
-        JTextArea textArea = new JTextArea();
-        textArea.setText(      message);
-        textArea.setEditable(false);;
-        textArea.setMargin(new Insets(PADDING, PADDING, PADDING, PADDING));
-        add(new JScrollPane(textArea), BorderLayout.CENTER);
 
-        
-        
-        final JDialog dialog = this;
-        JButton button = new JButton(
-            new AbstractAction("OK"){
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                dialog .dispose();
-            }
-        });
-        add(button, BorderLayout.SOUTH);
-    }
-    
-    protected AWTErrorDialog(String message){
-        this(message, DEFAULT_TITLE);
-    }
-    
-    /**
-     * Show a dialog with the provided message.
-     *
-     * @param message the message to display
-     */
-    public static void showDialog(String message) {
-        AWTErrorDialog dialog = new AWTErrorDialog(message);
-        dialog.setVisible(true);
-    }
+  public static String DEFAULT_TITLE = "Error in application";
+  public static int PADDING = 8;
+
+  /**
+   * Create a new Dialog with a title and a message.
+   *
+   * @param message the message to display
+   * @param title the title to display
+   */
+  protected AWTErrorDialog(String message, String title) {
+    setTitle(title);
+    setSize(new Dimension(600, 400));
+    setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+    setLocationRelativeTo(null);
+
+    Container container = getContentPane();
+    container.setLayout(new BorderLayout());
+
+    JTextArea textArea = new JTextArea();
+    textArea.setText(message);
+    textArea.setEditable(false);
+    textArea.setMargin(new Insets(PADDING, PADDING, PADDING, PADDING));
+    add(new JScrollPane(textArea), BorderLayout.CENTER);
+    final JDialog dialog = this;
+
+    JButton button = new JButton(
+      new AbstractAction("OK") {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+          dialog.dispose();
+        }
+      }
+    );
+    add(button, BorderLayout.SOUTH);
+  }
+
+  protected AWTErrorDialog(String message) {
+    this(message, DEFAULT_TITLE);
+  }
+
+  /**
+   * Show a dialog with the provided message.
+   *
+   * @param message the message to display
+   */
+  public static void showDialog(String message) {
+    AWTErrorDialog dialog = new AWTErrorDialog(message);
+    dialog.setVisible(true);
+  }
 }
