78b88c75a1f8a436899b41dc9a37c160d22be220
riccardobl <riccardobl@users.noreply.github.com>
2023-08-19 16:12:54 +0000

    auto-format

--- jme3-plugins/src/main/java/com/jme3/scene/plugins/IrMesh.java
+++ jme3-plugins/src/main/java/com/jme3/scene/plugins/IrMesh.java
@@ -32,12 +32,15 @@
 package com.jme3.scene.plugins;
 
 public class IrMesh {
-    
-    public IrPolygon []polygons;;    
-    public IrMesh deepClone() {
-        IrMesh m = new IrMesh();        m.polygons = new IrPolygon[polygons.length];
-        for (int i = 0; i < polygons.length;i++) {            m.polygons[i] = polygons[i].deepClone();
-        }
-        return m;
+
+  public IrPolygon[] polygons;
+
+  public IrMesh deepClone() {
+    IrMesh m = new IrMesh();
+    m.polygons = new IrPolygon[polygons.length];
+    for (int i = 0; i < polygons.length; i++) {
+      m.polygons[i] = polygons[i].deepClone();
     }
+    return m;
+  }
 }
