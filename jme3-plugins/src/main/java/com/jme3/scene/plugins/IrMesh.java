b70bc8b20823572a355c361a3b9db9b526ca801d
Riccardo Balbo <riccardo0blb@gmail.com>
2023-08-19 18:10:40 +0200

    Update IrMesh.java

--- jme3-plugins/src/main/java/com/jme3/scene/plugins/IrMesh.java
+++ jme3-plugins/src/main/java/com/jme3/scene/plugins/IrMesh.java
@@ -33,13 +33,10 @@ package com.jme3.scene.plugins;
 
 public class IrMesh {
     
-    public IrPolygon[] polygons;
-    
+    public IrPolygon []polygons;;    
     public IrMesh deepClone() {
-        IrMesh m = new IrMesh();
-        m.polygons = new IrPolygon[polygons.length];
-        for (int i = 0; i < polygons.length; i++) {
-            m.polygons[i] = polygons[i].deepClone();
+        IrMesh m = new IrMesh();        m.polygons = new IrPolygon[polygons.length];
+        for (int i = 0; i < polygons.length;i++) {            m.polygons[i] = polygons[i].deepClone();
         }
         return m;
     }
